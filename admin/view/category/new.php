fba06e646ec78b60e05bc252fdd772b4d198eb9f
leoMouraNet <laercio.zdq@gmail.com>
2016-06-29 17:06:38 -0400

    Category List

--- admin/view/category/new.php
+++ admin/view/category/new.php
@@ -1,5 +1,4 @@
       <div class="starter-template">
-<<<<<<< HEAD
         <h1>Register a new Category</h1>
       </div>
 		<form role="form" action="?route=category/list&method=insert" method="post">
@@ -12,38 +11,4 @@
         <input type="text" class="form-control" id="image" name="image" required>
       </div>  
 		  <button type="submit" class="btn btn-default">Add</button>
-=======
-        <h1>Register a new Student</h1>
-      </div>
-		<form role="form" action="?route=student/list&method=insert" method="post">
-      <div class="form-group">
-        <label for="first_name">Name:</label>
-        <input type="text" class="form-control" id="first_name" name="first_name" required>
-      </div>
-      <div class="form-group">
-        <label for="last_name">Last Name:</label>
-        <input type="text" class="form-control" id="last_name" name="last_name" required>
-      </div>  
-		  <div class="form-group">
-		    <label for="email">Email:</label>
-		    <input type="email" class="form-control" id="email" name="email" required>
-		  </div>
-		  <div class="form-group">
-		    <label for="pwd1">Password:</label>
-		    <input type="password" class="form-control" id="pwd1" name="pwd1" required>
-		  </div>
-		  <div class="form-group">
-		    <label for="pwd2">Re-Password:</label>
-		    <input type="password" class="form-control" id="pwd2" name="pwd2" required>
-		  </div>
-      <div class="form-group">
-        <label for="phone">Phone:</label>
-        <input type="text" class="form-control" id="phone" name="phone" required>
-      </div>  
-      <div class="form-group">
-        <label for="address">Address:</label>
-        <input type="text" class="form-control" id="address" name="address" required>
-      </div>  
-		  <button type="submit" class="btn btn-default">Register</button>
->>>>>>> 92cba0a4431ee2f9b5e3a867a69b7800c0eda229
 		</form>
