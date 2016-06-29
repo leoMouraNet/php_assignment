f0ca580a1fae06529692055e86c376ebbc84aafe
Simon Gonzalez <ximonali@gmail.com>
2016-06-28 22:31:05 -0400

    Statistics Workin
    showing summary of statistics
    
    we need to finish the Add and Edit test

--- admin/model/test/test_center.php
+++ admin/model/test/test_center.php
@@ -6,20 +6,6 @@ class ModelTestTestCenter extends Model {
 		return $test->rows;
 	}
 
-
-	function statusTest($test_id){
-		$enable = 1;
-		$disable = 0;
-
-		$actual_status = $this->query("SELECT status from test WHERE test_id = '" . (int)$test_id . "'");
-
-			if ($actual_status == 1){
-				$result = $this->query("UPDATE test set status = '" . $disable . "' WHERE test_id = '" . (int)$test_id . "'");
-			}else {
-				$result = $this->query("UPDATE test set status = '" . $enable . "' WHERE test_id = '" . (int)$test_id . "'");
-			}
-	}
-
 	function getTest($test_id) {
 		$test = $this->query("SELECT * FROM test WHERE test_id = '" . (int)$test_id . "'");
 		return $test->row;
@@ -58,6 +44,93 @@ class ModelTestTestCenter extends Model {
 			"', pass_score = '" .  $test['pass_score'] . 
 			"', time = '" .  $test['time'] . 
 			"', status = 1 WHERE test_id = '" . (int)$test['test_id'] . "'");
+	}
+
+	function statusTest($test_id){
+		$enable = 1;
+		$disable = 0;
+
+		$actual_status = $this->query("SELECT status from test WHERE test_id = '" . (int)$test_id . "'");
+
+			if ($actual_status == 1){
+				$result = $this->query("UPDATE test set status = '" . $disable . "' WHERE test_id = '" . (int)$test_id . "'");
+			}else {
+				$result = $this->query("UPDATE test set status = '" . $enable . "' WHERE test_id = '" . (int)$test_id . "'");
+			}
+	}
+
+//-------------------- Statistics ----------------------
+
+	function totalSummaryTest(){
+		//$passedScore = 80;
+		$userPassed = $this->query("SELECT * from student_test " .
+					" INNER JOIN test ON test.test_id = student_test.test_id " .
+					" INNER JOIN student ON student.student_id = test.test_id " .
+					" ORDER BY score DESC");
+		return $userPassed->rows;
+	}
+
+	// function totalNotPassedTest(){
+	// 	$passedScore = 80;
+	// 	$userNotPassed = $this->query("SELECT student.student_id,first_name,last_name, score from student " .
+	// 				" INNER JOIN student_test ON student.student_id = student_test.student_id " .
+	// 				" WHERE score < '" . (int)$passedScore . "' ORDER BY score ASC");
+	// 	return $userNotPassed->rows;
+	// }
+
+
+
+
+	function statisticsTest(){
+		$passedScore = 50;
+		$summary = $this->query("SELECT student.student_id,first_name,last_name, score from student " .
+					" INNER JOIN student_test ON student.student_id = student_test.student_id " .
+					" INNER JOIN test ON test.test_id = student_test.test_id " .
+					" WHERE score > '" . (int)$passedScore . "' ORDER BY score DESC");
+
+		return $summary->rows;
+
+	}
+
+	function userPassedTest(){
+		$userPassed = $this->query("SELECT COUNT(*) AS passed from student_test WHERE score > '80'");
+		return $userPassed->row;
+
+	}
+
+	function userNotPassedTest(){
+		$userNotPassed = $this->query("SELECT COUNT(*) AS notPassed from student_test WHERE score < '80'");
+		return $userNotPassed->row;
+	}
+
+
+	function avgScoreAttempted(){
+		$avgScoreAttempted = $this->query("SELECT AVG(score) as avgScore from student_test");
+		return $avgScoreAttempted->row;
+	}
+
+	function currentlyUsers(){
+		$currentlyUsers = $this->query("SELECT COUNT(*) AS actualUsers from student_test WHERE status = '1'");
+		return $currentlyUsers->row;	
+	}
+
+
+
+	function scoreTest(){
+		$testScoreByDate = 0;
+		$allTestScore = 0;
+		$highestScore = 0;
+		$lowestScore = 0;
+		$others_etc = 0;
+
+		$scoreArray = array();
+		$scoreArray['testScoreByDate'] = $testScoreByDate ;
+		$scoreArray['allTestScore'] = $allTestScore;
+		$scoreArray['highestScore'] = $highestScore;
+		$scoreArray['lowestScore'] = $lowestScore;
+
+		return $scoreArray;
+		
 	}
 
 }
--- admin/view/test/view_statistics.php
+++ admin/view/test/view_statistics.php
@@ -1,6 +1,78 @@
       <div class="starter-template">
         <h1>Test Statistics</h1>
-        <p class="lead">show current students taking test<br></p>
+        <p class="lead">show current test statistics </p>
       </div>
 
+
+<div class="container">
+      <h3>Summary Students Test Status</h3>
+      <br>
+        <table class="table table-bordered">
+          <thead align="left" style="display: table-header-group">
+          <tr class="info">
+                <th>Item</th>
+                <th>Course</th>
+                <th>Student</th>
+                <th>Score (pts)</th>
+                <th>Status</th>
+          </tr>
+          </thead>
+        <tbody>
+        <?php 
+        $total = 0;
+        foreach ($summaryTest as $rows) :?>
+          <tr class="item_row">
+                <td><?php echo ++$total; ?></td>
+                <td> <?php echo $rows['name']; ?></td>
+                <td> <?php echo $rows['first_name'] . " " . $rows['last_name']; ?></td>
+                <td> <?php echo $rows['score']; ?></td>
+                <!-- <td class="danger"> --> 
+                    <?php 
+                        if ($rows['score'] > 80){ 
+                          echo "<td class = success>";
+                          echo "pass";
+                          echo "</td>";
+                        }else {
+                          echo "<td class = danger>";
+                          echo "Fail";
+                          echo "</td>";
+                        } 
+
+                    ?>
+                <!-- </td> -->
+          </tr>
+        <?php endforeach;?>
+
+
+        </tbody>
+        </table>
+
+
+      </div>
+
+      <div class="container" id="resume">
+      <h3>Totals</h3>
+        <table class="table table-bordered">
+          <thead align="left" style="display: table-header-group">
+          <tr class="info">
+                <th>User Approved</th>
+                <th>User Not Approved</th>
+                <th>Avg Scores</th>
+                <th>Currently Writing Test</th>
+          </tr>
+          </thead>
+        <tbody>
+          <tr class="item_row">
+                <td><?php  echo $passedTest['passed']; ?></td>
+                <td> <?php echo $notPassedTest['notPassed']; ?></td>
+                <td> <?php echo $avgScoreAttempted['avgScore']; ?></td>
+                <td> <?php echo $currentlyUsers['actualUsers']; ?></td>
+          </tr>
+
+
+        </tbody>
+        </table>
+
+
+      </div>
 
